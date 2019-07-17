238a7106abb6d1e4021689d1e6516dd53ff1491d
Carter Currin <ccurrin@kochava.com>
2019-07-17 09:22:20 -0700

    Add async version of tract worker factory example.

--- example_tract_worker_test.go
+++ example_tract_worker_test.go
@@ -3,11 +3,12 @@ package tract_test
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"git.dev.kochava.com/ccurrin/tract"
 )
 
-func ExampleWorkerFactory_tractWorkerFactory() {
+func ExampleWorkerFactory_tractWorkerFactory_sync() {
 	squareRootTract := tract.NewWorkerTract("square root", 4, tract.NewFactoryFromWorker(SquareRootWorker{}))
 
 	factory := tract.NewTractWorkerFactory(squareRootTract)
@@ -15,6 +16,7 @@ func ExampleWorkerFactory_tractWorkerFactory() {
 	worker, err := factory.MakeWorker()
 	if err != nil {
 		//  Handle error
+		return
 	}
 
 	args := []float64{0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
@@ -24,7 +26,55 @@ func ExampleWorkerFactory_tractWorkerFactory() {
 		if !success {
 			fmt.Println("not successful")
 		}
-		result := resultReq.Value(SquareRootWorkerResult{}).(float64)
+		result, _ := resultReq.Value(SquareRootWorkerResult{}).(float64)
+		fmt.Println(result)
+	}
+
+	worker.Close()
+	factory.Close()
+
+	// Output:
+	// 0
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+	// 9
+	// 10
+}
+
+func ExampleWorkerFactory_tractWorkerFactory_async() {
+	squareRootTract := tract.NewWorkerTract("square root", 4, tract.NewFactoryFromWorker(SquareRootWorker{}))
+
+	factory := tract.NewTractWorkerFactory(squareRootTract)
+
+	worker, err := factory.MakeWorker()
+	if err != nil {
+		//  Handle error
+		return
+	}
+
+	args := []float64{0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
+	results := make([]float64, len(args))
+
+	wg := sync.WaitGroup{}
+	for i := range args {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			resultReq, success := worker.Work(context.WithValue(context.Background(), SquareRootWorkerArg{}, args[j]))
+			if !success {
+				fmt.Println("not successful")
+			}
+			results[j], _ = resultReq.Value(SquareRootWorkerResult{}).(float64)
+		}(i)
+	}
+	wg.Wait()
+	for _, result := range results {
 		fmt.Println(result)
 	}
 
